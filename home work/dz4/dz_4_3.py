aedb1eceec521c230f9b5f49f48c8a4954296823
Yuriy Makeev <biosxt@gmail.com>
2022-10-29 02:37:10 +0300

    До смены переменной слова

--- home work/dz4/dz_4_3.py
+++ home work/dz4/dz_4_3.py
@@ -3,13 +3,15 @@
 # яке однаково читається зліва направо та справа наліво. Наприклад, "випив".
 
 
+
+def reverse_string2(word):
+    word = "".join(reversed(word))
+    word2 = reverse_string2(word.lower())
+    if word.lower() == word2:
+        print(True)
+    else:
+        print(False)
+    return
+
 word = input('Введіть слово: ')
-
-def reverse_string2(s):
-    return "".join(reversed(s))
-
-word2 = reverse_string2(word.lower())
-if word.lower() == word2:
-    print(True)
-else:
-    print(False)
+print(reverse_string2(word))
