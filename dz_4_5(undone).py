e5d633f56b2af0551e6d92945c191d3fc8248df9
Люба <note.miloslav@mail.com>
2022-10-29 17:38:36 +0300

    Вроде рабочий вариант

--- dz_4_5(undone).py
+++ dz_4_5(undone).py
@@ -1,14 +1,25 @@
 
 # 5. Напишіть функцію, яка приймає рядок та повертає літеру, яка зустрічається В ньому найчастіше?
 
-# string = input("Задайте строку: ")
-string = "Задайте строку: "
-counted_chars = {}
+def letter_stat(string):
+    counted_chars = {}
+    for char in string:
+        if char in counted_chars:
+            counted_chars[char] += 1
 
-for char in string:
-    if char in counted_chars:
-        counted_chars[char] += 1
-    else:
-        counted_chars[char] = 1
+        else:
+            counted_chars[char] = 1
 
-print(max(counted_chars))
+    a = max(counted_chars.values())
+    for k, v in counted_chars.items():
+        if v >= a:
+            res = f"Літера {k}, зустрічається найчастіше ({v} рази)"
+            return res
+
+string = input("Задайте строку: ")
+string = string.replace(" ", "")
+string = string.replace(",", "")
+string = string.replace(".", "")
+string = string.replace("!", "")
+
+print(letter_stat(string))
