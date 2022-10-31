e82d337d759812fe1e0b81a90ec79924b628cf33
Люба <note.miloslav@mail.com>
2022-10-31 16:59:12 +0200

    Практика4

--- start.py
+++ start.py
@@ -6,7 +6,20 @@
 #     print(False)
 
 
-if False and True or 12<5**2 and "Python" > "Ruby":
+# if False and True or 12<5**2 and "Python" > "Ruby":
+#     print(True)
+# else:
+#     print(False)
+
+
+# if "Android" > "Apple" or "Apple" > "Android":
+#     print(True)
+# else:
+#     print(False)
+
+x = 10
+y = 15
+if y > x**y - 1000 or "test" > "push":
     print(True)
 else:
     print(False)
