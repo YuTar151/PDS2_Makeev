5623d8d8e7d2443b32cd72fdcd3e75103552c541
Люба <note.miloslav@mail.com>
2022-10-31 16:54:15 +0200

    Практика2

--- start.py
+++ start.py
@@ -1,6 +1,12 @@
 
 
-if "5a"<"5b" and 123/2 > 30 or True:
+# if "5a"<"5b" and 123/2 > 30 or True:
+#     print(True)
+# else:
+#     print(False)
+
+
+if False and True or 12<5**2 and "Python" > "Ruby":
     print(True)
 else:
     print(False)
