af53f4d93a3e1c27644ae669bbf2254010cc1532
Yuriy Makeev <biosxt@gmail.com>
2022-11-11 22:05:40 +0200

    Вигрузка ДЗ 10_1 + 10_2

--- home work/dz10/dz_10_1.py
+++ home work/dz10/dz_10_1.py
@@ -1,21 +1,21 @@
 """
 1. Напишіть функцію, яка б приймала номер місяця, а вертала його назву. Реалізуйте у функцiï декілька обробок виключень.
 """
-
 def mounth(num):
     mounth_dict = {1:"січень", 2:"лютий", 3:"березень", 4:"квітень", 5:"травень", 6:"червень", 7:"липень", 8:"серпень", 9:"Вересень", 10:"жовтень", 11:"листопад", 12:"грудень"}
-    if num <= 0 or num > 12:
-        print("Треба вказати число від 1 до 12")
-    else:
-        try:
+    try:
+        num = int(num)
+        if num <= 0 or num > 12:
+            print("Треба вказати число від 1 до 12")
+        else:
             print(mounth_dict.get(num))
-        except ValueError as ex:
-            return "Треба ввести ціле цисло"
-            print(ex)
-        except SyntaxError as ex:
-            return "Синтаксична помилка, спробуйте ще."
-            print(ex)
+    except ValueError as ex:
+        print("Треба ввести ціле цисло")
+        print(ex)
+    except SyntaxError as ex:
+        print("Синтаксична помилка, виправте та спробуйте ще.")
+        print(ex)
 
 
-num = int(input("Введіть номер місяця: "))
+num = input("Введіть номер місяця: ")
 mounth(num)
