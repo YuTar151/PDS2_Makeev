03b0921ea4ed9b2401d15bc6d7c77f6a9fd05c69
Yuriy Makeev <biosxt@gmail.com>
2022-11-27 20:56:36 +0200

    dz_11_3 edited

--- home work/dz11 (socket)/dz_11_3_server.py
+++ home work/dz11 (socket)/dz_11_3_server.py
@@ -24,35 +24,3 @@ try:
 
 except ConnectionAbortedError:
     print("Программа завершила роботу")
-
-
-
-
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# sock.bind(('', 55000))
-# sock.listen(10)
-# print('Server is running, please, press ctrl+c to stop')
-# conn, addr = sock.accept()
-# print('connected:', addr)
-#
-# def greetings():
-#
-#     greet = "Привіт. Напишіть мені якусь фразу, а я підрахую скільки там слів."
-#     conn.send(greet.encode('utf-8'))
-#     data = conn.recv(1024).decode('utf-8')
-#     word_counter = len(data.split(' '))
-#     greet = f"Ви відправили мені {word_counter} слів"
-#     conn.send(greet.encode('utf-8)')
-#
-# try:
-#     greetings()
-#
-# except KeyboardInterrupt as ex:
-#     print("Зв'язок завершено")
-#     print(ex)
-#
-# except ConnectionResetError as ex:
-#     print("Клієнт завершив зв'язок")
-#     print(ex)
-#
-# conn.close()
