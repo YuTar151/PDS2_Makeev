5fd15a1030b4c7f14a3ec3172b5be6a859e24cbe
Люба <note.miloslav@mail.com>
2022-11-29 20:38:55 +0200

    dz_12_2 edited

--- home work/dz12/dz_12_2_client.py
+++ home work/dz12/dz_12_2_client.py
@@ -4,15 +4,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('localhost', 55000))
 
 def send():
-    puncts = ".,!:;`?&@"
-    msg = input('Введіть текст: ')
-    for i in msg:
-        if i in puncts:
-            msg = msg.replace(i, "")
-    else:
-        sock.send(msg.encode("utf-8"))
-        data = sock.recv(1024).decode("utf-8")
-        print(data)
+    # msg = input('Введіть два числа: ')
+    msg = "1 12"
+    sock.send(msg.encode("utf-8"))
+    data = sock.recv(1024).decode("utf-8")
+    print(data)
 
 try:
     send()
--- home work/dz12/dz_12_2_server.py
+++ home work/dz12/dz_12_2_server.py
@@ -3,26 +3,40 @@
 прості арифметичні Функції - додавання, вiднiмання та множення - з використанням користувацьких функцій, які працюватимуть
 у асинхронному режимі.
 """
-
 import socket
 import asyncio
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('', 55000))
-sock.listen(10)
-print('Server is running, please, press ctrl+c to stop')
-conn, addr = sock.accept()
-print('connected:', addr)
+async def serv_run():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 55000))
+    sock.listen(10)
+    print('Server is running, please, press ctrl+c to stop')
+    conn, addr = sock.accept()
+    print('connected:', addr)
+    await asyncio.sleep(0)
 
-try:
 
-    while True:
-        data = conn.recv(1024).decode('utf-8')
-        word_counter = len(data.split(' '))
-        msg = f'Було надіслано {word_counter} слів'
-        conn.send(msg.encode('utf-8'))
+    try:
 
-    conn.close()
+        while True:
+            global counter
+            data = conn.recv(1024).decode('utf-8')
+            print('Client send: ', type(data))
+            counter = list(data.split(' '))
+            def sumf(counter):
+                res = 0
+                for i in counter:
+                    res += i
+                return res
 
-except ConnectionAbortedError:
-    print("Программа завершила роботу")
+            msg = str(f'Сумма вказанних чисел = {sumf(counter)}')
+            conn.send(msg.encode('utf-8'))
+
+        conn.close()
+
+    except ConnectionAbortedError:
+        print("Программа завершила роботу з кліентом")
+
+
+
+asyncio.run(serv_run())
