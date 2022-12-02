d9690f01d4ec51d87dcbcf0d6eec4d1760bf29f7
Люба <note.miloslav@mail.com>
2022-12-02 21:49:43 +0200

    dz_13 ready

--- home work/dz13/dz_13_2.py
+++ home work/dz13/dz_13_2.py
@@ -20,8 +20,6 @@ for i in range(0, 5000):
 
 
 def selection_sort(data):
-    global iter
-    iter = 0
     for scanIndex in range(0, len(data)):
         minIndex = scanIndex
         for compIndex in range(scanIndex + 1, len(data)):
@@ -29,58 +27,52 @@ def selection_sort(data):
                 minIndex = compIndex
         if minIndex != scanIndex:
             data[scanIndex], data[minIndex] = data[minIndex], data[scanIndex]
-            iter += 1
     print("Selection Sort:", data)
-    print("Iterations: ", iter)
 
 
-laps = 0
-Duration_time = 0
+def stat():
+    laps = 0
+    Duration_time = 0
 
-while laps < 10:
-    copy_my_list = int_list
-    cur_time = time.time()
-    selection_sort(copy_my_list)
-    print(f"Duration time (int_list): {time.time() - cur_time}")
-    laps += 1
-    Duration_time += time.time() - cur_time
-else:
-    sorting_int_list = Duration_time / 10
-    int_list_iter = copy.iter
+    while laps < 10:
+        copy_my_list = int_list
+        cur_time = time.time()
+        selection_sort(copy_my_list)
+        print(f"Duration time (int_list): {time.time() - cur_time}")
+        laps += 1
+        Duration_time += time.time() - cur_time
+    else:
+        sorting_int_list = Duration_time / 10
 
+    laps = 0
+    Duration_time = 0
 
+    while laps < 10:
+        copy_my_list = float_list
+        cur_time = time.time()
+        selection_sort(copy_my_list)
+        print(f"Duration time (float_list): {time.time() - cur_time}")
+        laps += 1
+        Duration_time += time.time() - cur_time
+    else:
+        sorting_float_list = Duration_time / 10
 
-laps = 0
-Duration_time = 0
+    laps = 0
+    Duration_time = 0
 
-while laps < 10:
-    copy_my_list = float_list
-    cur_time = time.time()
-    selection_sort(copy_my_list)
-    print(f"Duration time (float_list): {time.time() - cur_time}")
-    laps += 1
-    Duration_time += time.time() - cur_time
-else:
-    sorting_float_list = Duration_time / 10
-    float_list_iter = iter
-
-
-laps = 0
-Duration_time = 0
-
-while laps < 10:
-    copy_my_list = str_list
-    cur_time = time.time()
-    selection_sort(copy_my_list)
-    print(f"Duration time (str_list): {time.time() - cur_time}")
-    laps += 1
-    Duration_time += time.time() - cur_time
-else:
-    sorting_str_list = Duration_time / 10
-    str_list_iter = iter
-    print("Summary:")
-    print(f"Average time sorting int_list: {sorting_int_list}, iterations = {int_list_iter}")
-    print(f"Average time sorting float_list: {sorting_float_list}, iterations = {float_list_iter}")
-    print(f"Average time sorting str_list: {sorting_str_list}, iterations = {str_list_iter}")
+    while laps < 10:
+        copy_my_list = str_list
+        cur_time = time.time()
+        selection_sort(copy_my_list)
+        print(f"Duration time (str_list): {time.time() - cur_time}")
+        laps += 1
+        Duration_time += time.time() - cur_time
+    else:
+        sorting_str_list = Duration_time / 10
+        print("Summary:")
+        print(f"Average time sorting int_list: {sorting_int_list}")
+        print(f"Average time sorting float_list: {sorting_float_list}")
+        print(f"Average time sorting str_list: {sorting_str_list}")
 
 
+stat()
