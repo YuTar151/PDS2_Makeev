b5e2525be6cc86cbccb49c4ceb76c242b4cd5b7f
Dell Work <biosxt@gmail.com>
2023-03-24 17:51:29 +0200

    dz_26 done

--- tmp.py
+++ tmp.py
@@ -1,3 +1,13 @@
+from scipy.stats import binom
+
+n = 100
+p = 0.9
+lower_bound = 85
+upper_bound = 95
+
+prob = binom.cdf(upper_bound, n, p) - binom.cdf(lower_bound - 1, n, p)
+
+print("The probability that the number of televisions that last the warranty period will be within [85;95] is:", prob)
 
 
 
